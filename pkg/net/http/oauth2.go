efa516abaf02ef242f9942223f444eff33744be0
Gabe Conradi <gabe.conradi@gmail.com>
2021-07-17 15:44:28 -0500

    make a reusable google authed http client

---
+++ pkg/net/http/oauth2.go
@@ -0,0 +1,98 @@
+package http
+
+import (
+	"context"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/byxorna/jot/pkg/runtime"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+type Client struct {
+	sync.RWMutex
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func GetHTTPClient(ctx context.Context, oauth2cfg *oauth2.Config, tokenStorageFile string) (*http.Client, error) {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile, err := runtime.File(tokenStorageFile)
+	if err != nil {
+		return nil, fmt.Errorf("unable to determine token storage file %s: %w", tokenStorageFile, err)
+	}
+
+	tok, err := TokenFromFile(tokFile)
+	if err != nil {
+		tok, err := GetTokenFromWeb(ctx, oauth2cfg)
+		if err != nil {
+			return nil, err
+		}
+		SaveToken(tokFile, tok)
+	}
+
+	return oauth2cfg.Client(ctx, tok), nil
+}
+
+// Request a token from the web, then returns the retrieved token.
+func GetTokenFromWeb(ctx context.Context, oauth2cfg *oauth2.Config) (*oauth2.Token, error) {
+	authURL := oauth2cfg.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
+
+	// TODO: exec "open ..."
+	fmt.Printf("Please enter the auth code here: ")
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		return nil, fmt.Errorf("unable to read authorization code: %w", err)
+	}
+
+	tok, err := oauth2cfg.Exchange(ctx, authCode)
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve token from web: %w", err)
+	}
+	return tok, nil
+}
+
+// Retrieves a token from a local file.
+func TokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func SaveToken(path string, token *oauth2.Token) error {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("unable to cache oauth token: %w", err)
+	}
+	defer f.Close()
+	return json.NewEncoder(f).Encode(token)
+}
+
+// New creates a new http client that is authorized to use a given set of google API scopes
+// If modifying these scopes, delete your previously saved tokenStorageFile
+// tokenStorageFile will be used as a cache for the saved token
+func NewClientWithGoogleAuthedScopes(ctx context.Context, tokenStorageFile string, credentialsJSON []byte, scope ...string) (*http.Client, error) {
+	cfg, err := google.ConfigFromJSON(credentialsJSON, scope...)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse client secret file to config: %w", err)
+	}
+	httpclient, err := GetHTTPClient(ctx, cfg, tokenStorageFile)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create client: %w", err)
+	}
+
+	return httpclient, nil
+}
--- pkg/plugins/keep/client.go
+++ pkg/plugins/keep/client.go
@@ -1,19 +1,26 @@
-package calendar
+package keep
 
 import (
 	"context"
 	_ "embed"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
 	"path"
 	"sort"
 	"sync"
 	"time"
 
+	keep "google.golang.org/api/keep/v1"
+
 	"github.com/byxorna/jot/pkg/config"
 	"github.com/byxorna/jot/pkg/db"
-	"github.com/byxorna/jot/pkg/net/http"
+	"github.com/byxorna/jot/pkg/runtime"
 	"github.com/byxorna/jot/pkg/types"
 	v1 "github.com/byxorna/jot/pkg/types/v1"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
 )
@@ -21,59 +28,113 @@ import (
 var (
 	// ReconciliationDuration is how often to refresh events from the API
 	ReconciliationDuration = time.Minute * 10
-	pluginName             = config.PluginTypeCalendar
+	pluginName             = config.PluginTypeKeep
 	// This is sourced from setting up the oauth client somewhere like
 	// https://developers.google.com/calendar/caldav/v2/guide?hl=en_US
 	// TODO: idk whether its ok to package this into the repo or not!!!!
 	//go:embed credentials.json
 	credentialsJSON []byte
 
-	// maxEventsInDay is how many events we query from google calendar per day
-	maxEventsInDay int64 = 40
-
-	tokenStorageFile = "google_calendar_token.json"
-)
-
-const (
-	googlePrimaryCalendarID = "primary"
+	tokenStorageFile = "google_keep_token.json"
 )
 
 type Client struct {
 	sync.RWMutex
 	*calendar.Service
 
-	calendarIDs []string
 	status      v1.SyncStatus
-	eventList   []*Event
-	eventMap    map[types.DocIdentifier]*Event
 	lastFetched time.Time
 }
 
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(ctx context.Context, oauth2cfg *oauth2.Config) (*http.Client, error) {
+
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile, err := runtime.File(tokenStorageFile)
+	if err != nil {
+		return nil, fmt.Errorf("unable to determine token storage file %s: %w", tokenStorageFile, err)
+	}
+
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok, err := getTokenFromWeb(ctx, oauth2cfg)
+		if err != nil {
+			return nil, err
+		}
+		saveToken(tokFile, tok)
+	}
+
+	return oauth2cfg.Client(ctx, tok), nil
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(ctx context.Context, oauth2cfg *oauth2.Config) (*oauth2.Token, error) {
+	authURL := oauth2cfg.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("[plugin:%s] Go to the following link in your browser then type the authorization code: \n%v\n", pluginName, authURL)
+
+	fmt.Printf("[plugin:%s] Please enter the auth code here: ", pluginName)
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		return nil, fmt.Errorf("unable to read authorization code: %w", err)
+	}
+
+	tok, err := oauth2cfg.Exchange(ctx, authCode)
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve token from web: %w", err)
+	}
+	return tok, nil
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) error {
+	fmt.Printf("[plugin:%s] Saving credential file to: %s\n", pluginName, path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("unable to cache oauth token: %w", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+	return nil
+}
+
 func New(ctx context.Context, settings map[string]string, calendarIDs []string) (*Client, error) {
-	client, err := http.NewClientWithGoogleAuthedScopes(ctx, tokenStorageFile, credentialsJSON, calendar.CalendarEventsReadonlyScope)
+	// If modifying these scopes, delete your previously saved token.json.
+	cfg, err := google.ConfigFromJSON(credentialsJSON, calendar.CalendarReadonlyScope)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse client secret file to config: %w", err)
+	}
+	client, err := getClient(ctx, cfg)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create client: %w", err)
 	}
 
-	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+	srv, err := keep.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		return nil, fmt.Errorf("unable to retrieve Calendar client: %w", err)
 	}
 
-	if len(calendarIDs) == 0 {
-		calendarIDs = []string{googlePrimaryCalendarID}
-	}
-
 	c := Client{
-		Service:     srv,
-		calendarIDs: calendarIDs,
-		eventMap:    map[types.DocIdentifier]*Event{},
-		eventList:   []*Event{},
+		Service: srv,
 	}
 	return &c, nil
 }
 
-func (c *Client) DayEvents(t time.Time) ([]*Event, error) {
+/*
+* func (c *Client) DayEvents(t time.Time) ([]*Event, error) {
 	// search each calendar serially for the events
 	aggr := []*Event{}
 	for _, calID := range c.calendarIDs {
@@ -87,39 +148,10 @@ func (c *Client) DayEvents(t time.Time) ([]*Event, error) {
 	// TODO: filter this or otherwise order based on event time?
 	return aggr, nil
 }
-
-func (c *Client) dayEvents(t time.Time, calendarID string) ([]*Event, error) {
-	tMin := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC)
-	// TODO: use the working hours from config instead
-	tMax := time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 0, 0, time.UTC)
-
-	events, err := c.Service.Events.
-		List(calendarID).
-		ShowDeleted(false).
-		SingleEvents(true).
-		TimeMin(tMin.Format(time.RFC3339)).
-		TimeMax(tMax.Format(time.RFC3339)).
-		MaxResults(maxEventsInDay).
-		OrderBy("startTime").
-		Do()
-	if err != nil {
-		return nil, fmt.Errorf("unable to retrieve events from %s calendar: %w", calendarID, err)
-	}
-
-	calEntries := make([]*Event, len(events.Items))
-	for i, item := range events.Items {
-		evt, err := newEvent(calendarID, item)
-		if err != nil {
-			return nil, err
-		}
-		calEntries[i] = evt
-	}
-
-	return calEntries, nil
-}
+*/
 
 func (c *Client) DocType() types.DocType {
-	return types.CalendarEntryDoc
+	return types.KeepItemDoc
 }
 
 func (c *Client) Count() int {
@@ -211,25 +243,6 @@ func (c *Client) reconcileSingleEventBroken(id types.DocIdentifier) (db.Doc, err
 func (c *Client) hasEvent(e *Event) bool {
 	_, ok := c.eventMap[e.Identifier()]
 	return ok
-}
-
-func (c *Client) addToCollection(e *Event) {
-	// lock has already been claimed in exported functions
-	//
-	// insert into list if not present
-	if !c.hasEvent(e) {
-		c.eventMap[e.Identifier()] = e
-		c.eventList = append(c.eventList, e)
-	} else {
-		for i, x := range c.eventList {
-			if x.Identifier() == e.Identifier() {
-				c.eventList[i] = e
-				c.eventMap[e.Identifier()] = e
-			}
-		}
-	}
-	// stable sort list
-	sort.Stable(eventsByCreationDate(c.eventList))
 }
 
 func (c *Client) List() ([]db.Doc, error) {
